a3eebfd7f6961359e7aded82c599c9d5bbefbad5
jian-ge-tai-da <1519162203@qq.com>
2022-07-22 09:41:17 +0800

    face recognition init

---
+++ face_recognition/face_algorithm.py
@@ -0,0 +1,5 @@
+import cv2
+import face_recognition as fr
+
+if __name__ == '__main__':
+    pass
---
+++ face_recognition/main.py
@@ -0,0 +1,10 @@
+from face_recognition import *
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+    pass
